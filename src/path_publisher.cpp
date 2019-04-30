3b60ced0ad769887b1f40dc8c70635cb9136c6df
NiklasJang <niklasjang@gmailcom>
2019-04-30 21:43:53 +0900

    Make Go straight, Turn left, Turn Right

--- src/path_publisher.cpp
+++ src/path_publisher.cpp
@@ -5,62 +5,82 @@
 #include <geometry_msgs/Twist.h>
 #include "niklasjang_path_planning/MsgTutorial.h"
 
-int main(int argc, char **argv)// 노드 메인 함수
-{
-	
-	ros::init(argc, argv, "path_publisher"); // 노드명 초기화
+class MY_ROBOT{
+private :
+	geometry_msgs::Twist twist;
 	ros::NodeHandle nh; // ROS 시스템과 통신을 위한 노드 핸들 선언
-
-	// 퍼블리셔 선언, ros_tutorials_topic 패키지의 MsgTutorial 메시지 파일을 이용한
-	// 퍼블리셔 ros_tutorial_pub 를 작성한다. 토픽명은 "ros_tutorial_msg" 이며,
-	// 퍼블리셔 큐(queue) 사이즈를 100개로 설정한다는 것이다
 	ros::Publisher path_pub;// = nh.advertise<niklasjang_path_planning::MsgTutorial>("/path1/cmd_vel", 1000);
+public:
+	void Initialize(void);
+	void SetMsg(double _x, double _z){
+		twist.linear.x = _x;
+		twist.angular.z = _z;
+	}
+	void RollRoll(double spen);
+	
+	void GoStraight(void){
+		SetMsg(0.3, 0.0);
+		RollRoll(5);
+		Stop();
+	}
+
+	void TurnLeft(void){
+		SetMsg(0.0, +1.4);
+		RollRoll(1.0);
+		Stop();
+	}
+
+	void TurnRight(void){
+		SetMsg(0.0, -1.4);
+		RollRoll(1.0);
+		Stop();
+	}
+
+	void Stop(void){
+		SetMsg(0.0, 0.0);
+		RollRoll(0.2);
+	}
+};
+
+void MY_ROBOT::Initialize(void){
 	path_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
 	// 루프 주기를 설정한다. "10" 이라는 것은 10Hz를 말하는 것으로 0.1초 간격으로 반복된다
 	ros::Rate loop_rate(10);
-	
-	geometry_msgs::Twist twist;
-	twist.linear.x = 0.3;
-	twist.angular.z = 0.0;
+}
 
+void MY_ROBOT::RollRoll(double spen){
 	ros::Time beginTime = ros::Time::now();
 	double begin = beginTime.toSec();
-	ros::Duration secondsIWantToSendMessagesFor = ros::Duration(3); 
+	ros::Duration secondsIWantToSendMessagesFor = ros::Duration(spen); 
 	double end = secondsIWantToSendMessagesFor.toSec() + begin;
 	double current = 0.0;
 	while(current = ros::Time::now().toSec() < end )
 	{
 		current = ros::Time::now().toSec();
-		ROS_INFO("current : %f \n", current);
-		ROS_INFO("end : %f \n", end);
+		ROS_INFO("x, z : (%f, %f) \n", twist.linear.x, twist.angular.z);
+		//ROS_INFO("current : %f \n", current);
+		//ROS_INFO("end : %f \n", end);
 		path_pub.publish(twist);
 
 		// Time between messages, so you don't blast out an thousands of 
 		// messages in your 3 secondperiod
 		ros::Duration(0.1).sleep();
 	}
+}
 
-	/*
-	while (ros::ok()){//If this node is not kiiled, return true
-		geometry_msgs::Twist twist;
-		twist.linear.x = 0.3;
-		twist.angular.z = 0.0;
 
-		//Solution 1. Do not use msg.
-		double t_current = ros::Time::now().toSec();
-		double duration = t_current - t_begin ;
-		ROS_INFO("duration %f = %f - %f \n", duration, t_current, t_begin); 
-		if( duration > 3.0){ //ros::Duration(3.0).sleep();
-			twist.linear.x = 0.3;
-			twist.angular.z = 3.0;
-		}
-		ROS_INFO("publishing geometry/Twist x: %f, %f", twist.linear.x, twist.angular.z); // stamp.sec 메시지를 표시한다
-		//ROS_INFO("publishing geometry/Twist x: %d, %d", msg.target_x, msg.target_y); // stamp.sec 메시지를 표시한다
-		path_pub.publish(twist); // 메시지를 발행한다 	
-		loop_rate.sleep(); // 위에서 정한 루프 주기에 따라 슬립에 들어간다. If  time loop is not filled with this loop_rate
-		
-	}
-	*/
+
+int main(int argc, char **argv)// 노드 메인 함수
+{	
+	ros::init(argc, argv, "path_publisher"); // 노드명 초기화
+	MY_ROBOT robot;
+	robot.Initialize();
+	ros::Duration(2.0).sleep();
+	robot.GoStraight();
+	ros::Duration(2.0).sleep();
+	robot.TurnLeft();
+	ros::Duration(2.0).sleep();
+	robot.TurnRight();
 	
 	return 0;
 }
