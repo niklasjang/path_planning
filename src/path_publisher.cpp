5ab283f928eabe5b1b69cd5194ee5ce28972c2c4
NiklasJang <niklasjang@gmailcom>
2019-05-01 23:21:09 +0900

    Fix Segmentation fault (core dumped) using nh.subscribe(/result, 1000, &MY_ROBOT::msgCallback, this)

--- src/path_publisher.cpp
+++ src/path_publisher.cpp
@@ -22,8 +22,8 @@ private :
 	ros::NodeHandle nh; // ROS 시스템과 통신을 위한 노드 핸들 선언
 	ros::Publisher path_pub;// = nh.advertise<niklasjang_path_planning::MsgTutorial>("/path1/cmd_vel", 1000);
 	ros::Subscriber pddl_sub;
-	vector<std::string> ins;
-	vector <pair<std::string, pair<std::string, std::string> > > next;
+	vector<string> ins;
+	vector <pair<string, pair<string, string> > > next;
 	string pddl_result;
 public:
 	void Initialize(void);
@@ -55,15 +55,15 @@ public:
 		SetMsg(0.0, 0.0);
 		RollRoll(0.2);
 	}
-	void split_inst(vector<std::string> &arr, std::string& str, const std::string& delim);
-	void split_next(vector<pair<std::string, pair<std::string, std::string> > > &arr, std::string& str, const std::string& delim);
+	void split_inst(vector<string> &arr, string& str, const string& delim);
+	void split_next(vector<pair<string, pair<string, string> > > &arr, string& str, const string& delim);
 	void split(void);
 };
 
-void MY_ROBOT::split_inst(vector<std::string> &arr, std::string& str, const std::string& delim) {
+void MY_ROBOT::split_inst(vector<string> &arr, string& str, const string& delim) {
 	size_t pos = 0;		
-	std::string token;
-	while ((pos = str.find(delim)) != std::string::npos) { //delim ���ڸ� ã������
+	string token;
+	while ((pos = str.find(delim)) != string::npos) { //delim ���ڸ� ã������
 		token = str.substr(0, pos);  //ó������ pos ���̸�ŭ�� ���ڸ� token����
 		//cout << token << endl;
 		str.erase(0, pos + delim.length());
@@ -73,14 +73,14 @@ void MY_ROBOT::split_inst(vector<std::string> &arr, std::string& str, const std:
 	arr.push_back(str);
 }
 
-void MY_ROBOT::split_next(vector <pair<std::string, pair<std::string, std::string> > > &arr, std::string& str, const std::string& delim) {
+void MY_ROBOT::split_next(vector <pair<string, pair<string, string> > > &arr, string& str, const string& delim) {
 	size_t pos = 0;
-	std::string token;
+	string token;
 	int index = 0;
-	std::string obj;
-	std::string x_pos;
-	std::string y_pos;
-	while ((pos = str.find(delim)) != std::string::npos) { //delim ���ڸ� ã������
+	string obj;
+	string x_pos;
+	string y_pos;
+	while ((pos = str.find(delim)) != string::npos) { //delim ���ڸ� ã������
 		token = str.substr(0, pos);  //ó������ pos ���̸�ŭ�� ���ڸ� token����
 		//cout << token <<","<< index << endl;
 		if (index == 0) { obj = token; }
@@ -97,19 +97,19 @@ void MY_ROBOT::split_next(vector <pair<std::string, pair<std::string, std::strin
 
 void MY_ROBOT::msgCallback(const std_msgs::StringConstPtr& _pddl_result)
 {
-	//ROS_INFO("pddl_result is of type: %s", typeid(pddl_result).name()); 
-	//ROS_INFO("pddl_result-> is of type: %s", typeid(pddl_result->data.c_str()).name()); 
-	ROS_INFO(_pddl_result->data.c_str());
 	pddl_result = _pddl_result->data;
-	split();
+	ROS_INFO(pddl_result.c_str());
+	
+
 }
 
 void MY_ROBOT::Initialize(void){
 	MY_ROBOT my_robot;
 	path_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
 	// 루프 주기를 설정한다. "10" 이라는 것은 10Hz를 말하는 것으로 0.1초 간격으로 반복된다
-	pddl_sub = nh.subscribe("/result", 1000, &MY_ROBOT::msgCallback, &my_robot);
+	pddl_sub = nh.subscribe("/result", 1000, &MY_ROBOT::msgCallback, this);
 	ros::Rate loop_rate(10);
+	
 }
 
 void MY_ROBOT::RollRoll(double spen){
@@ -134,24 +134,26 @@ void MY_ROBOT::RollRoll(double spen){
 
 void MY_ROBOT::split(void){
 	int i = 0;
-	std::string pddl_result = "[ t7,x2,y2,move-left t6,x3,y2,move-down t3,x3,y1,move-right t7,x2,y1,move-up ]";
+	//string pddl_result = "[ t7,x2,y2,move-left t6,x3,y2,move-down t3,x3,y1,move-right t7,x2,y1,move-up ]";
 	string delimiter = " ";
 	
 	split_inst(ins, pddl_result, delimiter);
 	
-	ins.pop_back(); //�������� ������
+	ins.pop_back(); 
 	delimiter = ",";
-	for (int i = 1; i < ins.size(); i++) { //ó������ �ڸ��� �ʴ´�.
+	for (int i = 1; i < ins.size(); i++) { 
+		ROS_INFO("string is %s", ins[i].c_str());
 		//cout << ins[i] << "\n";
 		split_next(next, ins[i] , delimiter);
 	}
-	
+	ROS_INFO("next size is %d", next.size());
 	//cout << next.size() << "\n";
 	for (int i = 0; i < next.size(); i++) {
-		cout << "(" << next[i].first <<"," << next[i].second.first << "," << next[i].second.second << ")\n";
+		ROS_INFO("result 1: (%s, %s, %s)\n", next[i].first, next[i].second.first, next[i].second.second);
+		//cout << "(" << next[i].first <<"," << next[i].second.first << "," << next[i].second.second << ")\n";
 	}
-
-	cout << next[0].second.first << "\n";
+	ROS_INFO("result 2: (%s, %s, %s)\n", next[i].first, next[i].second.first, next[i].second.second);
+	//cout << next[0].second.first << "\n";
 }
 
 int main(int argc, char **argv)// 노드 메인 함수
