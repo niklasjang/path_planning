e31a581073c246adeed6cdb2cc959752f8080ed3
NiklasJang <niklasjang@gmailcom>
2019-05-02 00:39:01 +0900

    Subscribe PDDL result once

--- src/path_publisher.cpp
+++ src/path_publisher.cpp
@@ -58,36 +58,44 @@ public:
 	void split_inst(vector<string> &arr, string& str, const string& delim);
 	void split_next(vector<pair<string, pair<string, string> > > &arr, string& str, const string& delim);
 	void split(void);
+	vector <pair<string, pair<string, string> > >  GetNext(void){
+		return next;
+	}
+	void control(void);
 };
 
 void MY_ROBOT::split_inst(vector<string> &arr, string& str, const string& delim) {
+	//ROS_INFO("SPLIT INST");
 	size_t pos = 0;		
 	string token;
 	while ((pos = str.find(delim)) != string::npos) { //delim ���ڸ� ã������
 		token = str.substr(0, pos);  //ó������ pos ���̸�ŭ�� ���ڸ� token����
-		//cout << token << endl;
+		//ROS_INFO(token.c_str());
 		str.erase(0, pos + delim.length());
-		arr.push_back(token);
+		ins.push_back(token);
 	}
-	//cout << str<< endl;
+	//ROS_INFO(token.c_str());
 	arr.push_back(str);
 }
 
 void MY_ROBOT::split_next(vector <pair<string, pair<string, string> > > &arr, string& str, const string& delim) {
+	//ROS_INFO("SPLIT NEXT");
 	size_t pos = 0;
 	string token;
 	int index = 0;
 	string obj;
 	string x_pos;
 	string y_pos;
-	while ((pos = str.find(delim)) != string::npos) { //delim ���ڸ� ã������
-		token = str.substr(0, pos);  //ó������ pos ���̸�ŭ�� ���ڸ� token����
-		//cout << token <<","<< index << endl;
+	while ((pos = str.find(delim)) != string::npos) { 
+		token = str.substr(0, pos);  
+		//ROS_INFO(token.c_str());
+		//ROS_INFO("index is %d", index);
 		if (index == 0) { obj = token; }
 		if (index == 1) { x_pos = token;}
 		if (index == 2) {
 			y_pos = token;
-			arr.push_back(make_pair(obj, make_pair(x_pos, y_pos)));
+			//ROS_INFO(obj.c_str(), x_pos.c_str(), y_pos.c_str());
+			next.push_back(make_pair(obj, make_pair(x_pos, y_pos)));
 		}
 		str.erase(0, pos + delim.length());
 		index++;
@@ -96,11 +104,11 @@ void MY_ROBOT::split_next(vector <pair<string, pair<string, string> > > &arr, st
 }
 
 void MY_ROBOT::msgCallback(const std_msgs::StringConstPtr& _pddl_result)
-{
+{	
+	//ROS_INFO("MSGCALLBACK");
 	pddl_result = _pddl_result->data;
-	ROS_INFO(pddl_result.c_str());
-	
-
+	//ROS_INFO("Subscribe data : %s", pddl_result.c_str());
+	split();
 }
 
 void MY_ROBOT::Initialize(void){
@@ -108,8 +116,6 @@ void MY_ROBOT::Initialize(void){
 	path_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
 	// 루프 주기를 설정한다. "10" 이라는 것은 10Hz를 말하는 것으로 0.1초 간격으로 반복된다
 	pddl_sub = nh.subscribe("/result", 1000, &MY_ROBOT::msgCallback, this);
-	ros::Rate loop_rate(10);
-	
 }
 
 void MY_ROBOT::RollRoll(double spen){
@@ -121,7 +127,7 @@ void MY_ROBOT::RollRoll(double spen){
 	while(current = ros::Time::now().toSec() < end )
 	{
 		current = ros::Time::now().toSec();
-		ROS_INFO("x, z : (%f, %f) \n", twist.linear.x, twist.angular.z);
+		//ROS_INFO("x, z : (%f, %f) \n", twist.linear.x, twist.angular.z);
 		//ROS_INFO("current : %f \n", current);
 		//ROS_INFO("end : %f \n", end);
 		path_pub.publish(twist);
@@ -133,6 +139,7 @@ void MY_ROBOT::RollRoll(double spen){
 }
 
 void MY_ROBOT::split(void){
+	//ROS_INFO("SPILT");
 	int i = 0;
 	//string pddl_result = "[ t7,x2,y2,move-left t6,x3,y2,move-down t3,x3,y1,move-right t7,x2,y1,move-up ]";
 	string delimiter = " ";
@@ -142,26 +149,37 @@ void MY_ROBOT::split(void){
 	ins.pop_back(); 
 	delimiter = ",";
 	for (int i = 1; i < ins.size(); i++) { 
-		ROS_INFO("string is %s", ins[i].c_str());
+		//ROS_INFO("string is %s", ins[i].c_str());
 		//cout << ins[i] << "\n";
 		split_next(next, ins[i] , delimiter);
 	}
-	ROS_INFO("next size is %d", next.size());
+	//ROS_INFO("next size is %d", next.size());
 	//cout << next.size() << "\n";
-	for (int i = 0; i < next.size(); i++) {
-		ROS_INFO("result 1: (%s, %s, %s)\n", next[i].first, next[i].second.first, next[i].second.second);
-		//cout << "(" << next[i].first <<"," << next[i].second.first << "," << next[i].second.second << ")\n";
-	}
-	ROS_INFO("result 2: (%s, %s, %s)\n", next[i].first, next[i].second.first, next[i].second.second);
+	//for (int i = 0; i < next.size(); i++) {
+	//	ROS_INFO("result 1:",  next[i].first.c_str(),  next[i].second.first.c_str(), next[i].second.second.c_str());
+	//	//cout << "(" << next[i].first <<"," << next[i].second.first << "," << next[i].second.second << ")\n";
+	//}
 	//cout << next[0].second.first << "\n";
 }
 
+
+void MY_ROBOT::control(void){
+	//ROS_INFO("CONTROL");
+	for (int i = 0; i < next.size(); i++) {
+		ROS_INFO(next[i].first.c_str());
+		ROS_INFO(next[i].second.first.c_str());
+		ROS_INFO(next[i].second.second.c_str());
+	}
+}
 int main(int argc, char **argv)// 노드 메인 함수
 {	
 	ros::init(argc, argv, "path_publisher"); // 노드명 초기화
 	
 	MY_ROBOT robot;
 	robot.Initialize();
+	ros::Rate loop_rate(10);
+
+	
 	/*
 	ros::Duration(2.0).sleep();
 	robot.GoStraight();
@@ -170,7 +188,15 @@ int main(int argc, char **argv)// 노드 메인 함수
 	ros::Duration(2.0).sleep();
 	robot.TurnRight();
 	*/
-
+	while (ros::ok())
+	{
+	  	ros::spinOnce();
+	  	loop_rate.sleep();
+	  	if(robot.GetNext().size() != 0){
+			robot.control();
+			return 0;	
+	  	}
+	}
 	ros::spin();
 }
 
