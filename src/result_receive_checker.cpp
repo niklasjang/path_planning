8782c7ed40220fb56ffc61888f369a4839f72397
NiklasJang <niklasjang@gmailcom>
2019-06-18 20:12:41 +0900

    Change result_receive_checker to use Three Roomba

--- src/result_receive_checker.cpp
+++ src/result_receive_checker.cpp
@@ -6,7 +6,7 @@ using namespace std;
 Callback : Get #Target1 PDDL_Result 
 */
 
-bool check[4];
+bool check[3];
 
 void r1ReceiveChecker(const std_msgs::StringConstPtr& received)
 {	
@@ -27,7 +27,7 @@ void r2ReceiveChecker(const std_msgs::StringConstPtr& received)
 		check[2] = true;
 	}
 }
-
+/*
 void r3ReceiveChecker(const std_msgs::StringConstPtr& received)
 {	
 	ROS_INFO("CATCH OK R3 RESULT ");
@@ -37,7 +37,7 @@ void r3ReceiveChecker(const std_msgs::StringConstPtr& received)
 		check[3] = true;
 	}
 }
-
+*/
 /*
 void r4ReceiveChecker(const std_msgs::StringConstPtr& received)
 {	
@@ -61,7 +61,7 @@ int main(int argc, char **argv)// 노드 메인 함수
 
 	receive_sub[1] = nh.subscribe("/r1/received", 100, r1ReceiveChecker);
 	receive_sub[2] = nh.subscribe("/r2/received", 100, r2ReceiveChecker);
-	receive_sub[3] = nh.subscribe("/r3/received", 100, r3ReceiveChecker);
+	//receive_sub[3] = nh.subscribe("/r3/received", 100, r3ReceiveChecker);
 	//receive_sub[4] = nh.subscribe("/r4/received", 100, r4ReceiveChecker);
 	run_pub = nh.advertise<std_msgs::String>("/run", 100);
 
@@ -70,17 +70,20 @@ int main(int argc, char **argv)// 노드 메인 함수
 	{	
 		//ROS_INFO("size %d",rontroller.GetNext().size());
 		ROS_INFO("Waiting for All Result Reived");
-		for( int i=1; i<4; i++){
-			flag = flag && check[i];
-		}
-		if(flag){
-			std_msgs::String msg;
-			string s = "RUN";
-			msg.data = s;
-			run_pub.publish(msg);
-			for(int i=1; i<4; i++){
-				check[i] = false;
+		if(check[1] ==  true){
+			ROS_INFO("**r1 CHECK!!**");
+			if(check[2] == true){
+				ROS_INFO("**r2 CHECK!!**");
+				std_msgs::String msg;
+				string s = "RUN";
+				msg.data = s;
+				ROS_INFO("**RUN**");
+				run_pub.publish(msg);
+				for(int i=1; i<3; i++){
+					check[i] = false;
+				}
 			}
+			
 		}
 	  	ros::spinOnce();  
 	  	loop_rate.sleep(); //반복 구독을 수행하고 callback을 지속적으로 요청합니다.
